c362eda4988b0fded560d8e8d4b1050c7ade417d
Megan Dotto <megan.dotto@gmail.com>
2018-03-24 21:38:34 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  new_array = []
+  attendees.each do |name|
+  new_array >> badge_maker(name)
+  end
+    return new_array
+  end
+
+def assign_rooms(attendees)
+  room_number = []
+  attendees.each_with_index do |name, index|
+  room_number << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+end
+  return room_number
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |attendee|
+    puts attendee
+  end
+  assign_rooms(attendees).each do |attendee|
+    puts attendee
+  end
+end
